52423a0487b1c1862e0bf848281ade487320b343
Kuro Hsu <kurotanshi@gmail.com>
2020-08-23 01:21:26 +0800

    加入地圖與初始化

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="ubike-map">
-    <h1>MAP</h1>
-  </div>
+  <div id="ubike-map"></div>
 </template>
 
 <script>
+import L from 'leaflet';
+
 export default {
-  name: 'Map'
+  name: 'Map',
+  methods: {
+    mapInit () {
+      // 預設以台北車站經緯度為中心
+      const center = [25.046273, 121.517498]
+
+      // 對應 id="ubike-map"
+      this.map = L.map('ubike-map', { center: center, zoom: 14 });
+
+      // 版權宣告
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '<a target="_blank" href="https://www.openstreetmap.org/">© OpenStreetMap 貢獻者</a>',
+        maxZoom: 18,
+      }).addTo(this.map);
+    }
+  },
+  mounted () {
+    this.mapInit();
+  },
 }
 </script>
