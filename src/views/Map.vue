8645752939abdb9f93c7113e5924ac50bccdf8cc
Kuro Hsu <kurotanshi@gmail.com>
2020-08-23 02:00:53 +0800

    站點繪製

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -24,9 +24,41 @@ export default {
     }
   },
   watch: {
-
+    ubikeStops (stops) {
+      this.markerRepaint(stops);
+    }
   },
   methods: {
+    addMarker(stop) {
+      const map = this.map;
+
+      const popupHTML = `<div class="popup-box">
+          <h3>${stop.sna}</h3>
+          <p>站點位置 : ${stop.ar}</p>
+          <p>總車輛 : ${stop.tot} 輛</p>
+          <p>目前車輛 : ${stop.sbi} 輛</p>
+          <p>更新時間 : ${stop.mday}</p>
+        </div>`;
+
+      L.marker([stop.lat, stop.lng])
+        .addTo(map)
+        .bindPopup(popupHTML);
+    },
+    clearMarkers() {
+      if( this.map === null ) return;
+
+      this.map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          this.map.removeLayer(layer);
+        }
+      });
+    },
+    markerRepaint(stops = this.ubikeStops) {
+      if( this.map !== null ) {
+        this.clearMarkers();
+        stops.forEach((element) => this.addMarker(element) );
+      }
+    },
     mapInit () {
       // 預設以台北車站經緯度為中心
       const center = [25.046273, 121.517498]
